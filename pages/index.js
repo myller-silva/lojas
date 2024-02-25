48b5bb5adbb85306b2dfeb8158150dac57f41ea8
mylle <myllersilva1310@gmail.com>
2024-02-25 16:00:05 -0300

    update

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import Link from "next/link";
-import styles from '../styles/Home.module.css';
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.main}>
+      <h1>Ola mundo</h1>
       <div>
         <Link href="/login">Sign in</Link>
       </div>
